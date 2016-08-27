4886a2bc50c0de09b015613d14847274dea498d2
sawfly <eugene.sawfly@gmail.com>
2016-08-27 07:55:58 +0300

    Update ConnectionEvent.php

--- src/Events/ConnectionEvent.php
+++ src/Events/ConnectionEvent.php
@@ -1,1 +1,43 @@
+<?php
 
+namespace Sawfly\Visitors\Events;
+
+use App\Events\Event;
+use Illuminate\Http\Request;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class ConnectionEvent extends Event
+{
+    use SerializesModels;
+
+    private $request;
+
+    /**
+     * ConnectionEvent constructor.
+     * @param Request $request
+     */
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+        return $this;
+    }
+
+    /**
+     * Get the channels the event should be broadcast on.
+     *
+     * @return array
+     */
+    public function broadcastOn()
+    {
+        return [];
+    }
+
+    /**
+     * @return Request
+     */
+    public function getRequest()
+    {
+        return $this->request;
+    }
+}
